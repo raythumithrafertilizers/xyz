d2f0eb66d5304348e257138b3bd28c5958384951
Your Name <you@example.com>
2016-04-26 17:45:45 +0000

    done

--- backup_scripts/backup.sh
+++ backup_scripts/backup.sh
@@ -7,7 +7,7 @@
 
 
 # Other options
- backup_path="/home/satya/Desktop/BaseModule/Backup-To-Google-Drive/database_backup"
+ backup_path="/home/ubuntu/raythumithra/backup_scripts/database_backup"
  date=$(date +"%d-%b-%Y")
 
 # Set default file permissions
@@ -20,7 +20,13 @@
 # Delete files older than 30 days
  find $backup_path/* -mtime +30 -exec rm {} \;
 
-python backup.py config_file.json database_backup/$db_name.sql
-zip -r backup_bills/bill_images.zip /home/satya/Desktop/BaseModule/static/static/uploads
+python /home/ubuntu/raythumithra/backup_scripts/backup.py config_file.json database_backup/$db_name.sql
 
-python backup.py config_file2.json backup_bills/bill_images.zip
+zip -r /home/ubuntu/raythumithra/backup_scripts/backup_bills/bill_images.zip /home/ubuntu/raythumithra/static/static/uploads
+
+python /home/ubuntu/raythumithra/backup_scripts/backup.py config_file2.json backup_bills/bill_images.zip
+
+
+rm -rf /home/ubuntu/raythumithra/backup_scripts/backup_bills/*
+
+rm -rf /home/ubuntu/raythumithra/backup_scripts//database_backup/*
--- installations_r.sh
+++ installations_r.sh
@@ -12,5 +12,10 @@ sudo pip install --upgrade google-api-python-client
 
 sudo apt-get install nginx
 
+sudo apt-get install  zip
+
+sudo pip install google-api-python-client==1.4.2
+sudo pip install oauth2client==1.4.12
+
 echo "create database raythumithra" | mysql -u root -p
 
